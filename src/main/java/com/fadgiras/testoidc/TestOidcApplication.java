cdeb6a7bda696fac14ff7e5dcf96cb79b863a5eb
Fadgiras <mayeuxgudinaflorian@gmail.com>
2023-12-20 15:30:38 +0100

    First draft

---
+++ src/main/java/com/fadgiras/testoidc/TestOidcApplication.java
@@ -0,0 +1,13 @@
+package com.fadgiras.testoidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOidcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fadgiras/testoidc/TestOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadgiras.testoidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
